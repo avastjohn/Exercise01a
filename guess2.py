a204066ee08826e54c61d4d276b8ddde25089ebd
Student <user@chromebox-004.(none)>
2013-09-24 17:24:54 -0700

    Added check for integer

--- guess2.py
+++ guess2.py
@@ -4,18 +4,30 @@ print "Hi. What's your name?"
 name = raw_input("> ")
 print "Well, %s, I have a number between 1 and 100. Care to guess?" % name
 
-guess = int(raw_input())
-number = randint(1, 101)
-guess_count = 0
+def number_game():
+    guess = raw_input()
+    number = randint(1, 101)
+    guess_count = 0
 
-while number != guess:
-    if guess < number:
-        print "Nope. Guess higher."
+    if guess.isdigit():
+        while number != int(guess):
+            if int(guess) < number:
+                print "Nope. Guess higher."
+            else:
+                print "Nope. Guess lower."
+            guess = raw_input()
+            guess_count += 1
+
+        print "That's correct! My number was %d" % number
+        print "It only took you %d guesses!" % guess_count
     else:
-        print "Nope. Guess lower."
-    guess = int(raw_input())
-    guess_count += 1
+        guess = raw_input("I asked for a number.  Please type in a number. ")
 
 
-print "That's correct! My number was %d" % number
-print "It only took you %d guesses!" % guess_count
+number_game()
+print " "
+play_again = raw_input("Play again? y/n")
+
+if play_again == "y":
+    print "Ok! I've got a new number. Guess away!"
+    number_game()
